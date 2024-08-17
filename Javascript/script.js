4e311840c873d5780b93875a6c32999300db91f4
MrDongPhets <140035755+MrDongPhets@users.noreply.github.com>
2024-08-17 13:57:35 +0800

    new commit

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function () {
   $("nav a").on("click", function (event) {
     event.preventDefault();
@@ -19,4 +18,40 @@ $(document).ready(function () {
   $("#close").click(function () {
     $("#ul").removeClass("open-menu").addClass("close-menu");
   });
+
+  $("#instagram").on("click", function () {
+    window.open("https://www.instagram.com/christdodong/", "_blank");
+  });
+
+  $("#facebook").on("click", function () {
+    window.open(
+      "  https://www.facebook.com/profile.php?id=100010285125943",
+      "_blank"
+    );
+  });
+  $("#linked").on("click", function () {
+    window.open(
+      "https://www.linkedin.com/in/christian-mendoza-20bb97314/",
+      "_blank"
+    );
+  });
+
+$("#circle").click(function () {
+  $("#sidebar").removeClass("sidebar-right").addClass("sidebar");
+});
+
+$("#sidebar-close").click(function () {
+  $("#sidebar").removeClass("sidebar").addClass("sidebar-right");
+});
+
+$(window).scroll(function () {
+  var scroll = $(window).scrollTop();
+  if (scroll > 50) {
+    $("#circle").fadeIn();
+  } else {
+    $("#circle").fadeOut();
+    // Optionally hide the sidebar when scroll is less than 50
+    // $("#sidebar").removeClass("sidebar").addClass("sidebar-right");
+  }
+});
 });
