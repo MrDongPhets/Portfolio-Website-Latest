f6bce97760b14c31cec1b0e07c9997ba83fe9b5b
MrDongPhets <140035755+MrDongPhets@users.noreply.github.com>
2024-08-18 17:34:06 +0800

    adding resume

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,15 +1,19 @@
 $(document).ready(function () {
-  $("nav a").on("click", function (event) {
-    event.preventDefault();
+ $("nav a").on("click", function (event) {
+   // Check if href starts with '#' to determine if it's a local anchor
+   if ($(this).attr("href").startsWith("#")) {
+     event.preventDefault();
 
-    const target = $(this).attr("href"); //target section
-    $("html, body").animate(
-      {
-        scrollTop: $(target).offset().top, //scrolling
-      },
-      800
-    );
-  });
+     const target = $(this).attr("href"); // Target section
+     $("html, body").animate(
+       {
+         scrollTop: $(target).offset().top, // Scrolling
+       },
+       800
+     );
+   }
+ });
+
 
   $("#open").click(function () {
     $("#ul").removeClass("close-menu").addClass("open-menu");
